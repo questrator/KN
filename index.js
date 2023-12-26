938c3b15adae114cdbccd07abebbd5317944f64e
Slava Ivanov <drumtrack@ya.ru>
2023-12-26 18:32:43 +0300

    upd

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const settings = {
     winSeq: 3,
     players: 2    
 };
-
-const matchSeq = new RegExp("X".repeat(settings.winSeq));
+const x = `(\\(\\d+\\)){${settings.winSeq},}`;
+const matchSeq = new RegExp(x, "g");
 
 const fieldBlock = document.querySelector(".field");
 fieldBlock.style.gridTemplateColumns = `repeat(${settings.cols}, 1fr)`;
@@ -47,7 +47,7 @@ function clickField(event) {
 
         field.empty--;
         ["col", "row", "ad", "dd"].map(e => field[field.player][e][cell[e]])
-        .forEach(e => e[e.indexOf(+cell.id)] = "X");
+        .forEach(e => e[e.indexOf(+cell.id)] = `(${e[e.indexOf(+cell.id)]})`);
 
         field.win = checkWinSeq(event.target.dataset);
         if (field.win) {
@@ -61,6 +61,7 @@ function clickField(event) {
         }
         else changePlayer();
     }
+    console.log(field)
 }
 
 function changePlayer() {
@@ -71,5 +72,20 @@ function changePlayer() {
 function checkWinSeq(cell) {
     if (field.empty > totalCells - (settings.winSeq - 1) * settings.players) return false;
     return ["col", "row", "ad", "dd"].map(e => field[field.player][e][cell[e]])
-    .map(e => matchSeq.test(e.join(""))).some(e => e === true);
+    .map(e => {
+        const match = e.join("").match(matchSeq);
+        if (match != null) {
+            const line = match[0].replaceAll(")(", "-").slice(1,-1).split("-");
+            console.log(line);
+            mark(line);
+        }
+        return matchSeq.test(e.join(""));
+    }).some(e => e === true);
+}
+
+function mark(line) {
+    const cells = line.map(e => document.querySelector(`.cell[data-id="${e}"]`)).forEach(e => {
+        e.classList.add("win-line");
+    });
+    console.log(cells);
 }
